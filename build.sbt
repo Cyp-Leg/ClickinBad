1386949215578fbdc51fbff891277a7ee71e0bb5
FlorentBerland <florent.berland@laposte.net>
2018-10-23 17:07:56 +0200

    Fix the bugs

--- build.sbt
+++ build.sbt
@@ -6,4 +6,4 @@ scalaVersion := "2.11.0"
 
 libraryDependencies += "org.apache.spark" %% "spark-core" % "2.3.2"
 
-libraryDependencies += "org.apache.spark" %% "spark-mllib" % "2.3.2" % "runtime"
+libraryDependencies += "org.apache.spark" %% "spark-mllib" % "2.3.2"
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,17 @@
-object Main extends App {
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession
 
+object Main {
 
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession
+      .builder()
+      .appName("Spark SQL basic example")
+      .config(new SparkConf().setMaster("local").setAppName("AAAAA"))
+      .getOrCreate()
 
+    val ds = spark.read.json("C:\\Users\\Public\\Documents\\data-students.json")
+
+    ds.printSchema
+  }
 }
