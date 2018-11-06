ae83a451b9401c252b6a0cc17a6addee6cb3f41a
Cyprien Legrand <cyprien_legrand@hotmail.fr>
2018-11-06 22:21:19 +0100

    Preprocess beginning

--- build.sbt
+++ build.sbt
@@ -4,6 +4,10 @@ version := "0.1"
 
 scalaVersion := "2.11.0"
 
-libraryDependencies += "org.apache.spark" %% "spark-core" % "2.3.2"
-
-libraryDependencies += "org.apache.spark" %% "spark-mllib" % "2.3.2"
+libraryDependencies ++= Seq(
+    "org.apache.spark" %% "spark-core" % "2.3.2",
+    "org.apache.spark" %% "spark-sql" % "2.0.1",
+    "org.apache.spark" %% "spark-mllib-local" % "2.3.0",
+    "joda-time" % "joda-time" % "2.9.9",
+    "org.apache.spark" %% "spark-mllib" % "2.0.1"
+)
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,17 +1,54 @@
-import org.apache.spark.SparkConf
+import org.apache.spark.ml.Pipeline
+import org.apache.spark.ml.feature.{StringIndexer, OneHotEncoder, VectorAssembler}
+import org.apache.spark.ml.classification.LogisticRegression
+import org.apache.spark.sql.DataFrame
+import scala.util.matching.Regex
+import org.joda.time.format.DateTimeFormat
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.{udf, when, col}
 
-object Main {
+object PredictClick {
+  /* ----------------------------------------- */
+  /* -------------- Preprocessing ------------ */
+  /* ----------------------------------------- */
+    def preprocess(df: DataFrame): DataFrame = {
+        val network = udf { // getting countries from networks
+            (str: String) =>
+                val fr = new Regex("208-(.*)")
+                val can = new Regex("302-(.*)")
+                val es = new Regex("214-(.*)")
+                val tur = new Regex("286-(.*)")
+                val ger = new Regex("262-(.*)")
 
-  def main(args: Array[String]): Unit = {
-    val spark = SparkSession
-      .builder()
-      .appName("Spark SQL basic example")
-      .config(new SparkConf().setMaster("local").setAppName("AAAAA"))
-      .getOrCreate()
+                str match {
+                case fr(x) => "France"
+                case null | "Other" | "Unknown" => "Unknown"
+                case can(x) => "Canada"
+                case es(x) => "Espagne"
+                case tur(x) => "Turquie"
+                case ger(x) => "Allemagne"
+                case _ => "Unknown"
+            }
+        }
 
-    val ds = spark.read.json("C:\\Users\\Public\\Documents\\data-students.json")
+        // Replacing null values by "Unknown"s
+        val unknown = df.na.fill(Map("city" -> "Unknown","impid" -> "Unknown","interests" -> "Unknown","network" -> "Unknown","type" -> "Unknown"))
 
-    ds.printSchema
-  }
+        // Removing all add with size null
+        val ds2 = unknown.filter(!col("size").getItem(0).isNull).filter(!col("size").getItem(1).isNull)
+
+        // Gathering all windows phones together, set nulls & "Other" to "Unknown"
+        val os = udf {
+            (str: String) =>
+                val windows_pattern = new Regex("Windows(.*)")
+                
+                str match {
+                case windows_pattern(x) => "Windows Phone"
+                case null | "Other" | "Unknown" => "Unknown"
+                case x => x.toLowerCase.capitalize
+            }
+        }
+
+        ds2 // return clean dataframe
+    }
 }
