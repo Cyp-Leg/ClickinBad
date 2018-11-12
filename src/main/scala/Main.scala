d4e99b4f9a8c07afeca3b37afb1c139cf951c918
Cyprien Legrand <cyprien_legrand@hotmail.fr>
2018-11-12 22:48:30 +0100

    UDFs crashing

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -27,33 +27,39 @@ object clickinBad {
                 case es(x) => "Espagne"
                 case tur(x) => "Turquie"
                 case ger(x) => "Allemagne"
-                case _ => "Unknown"
             }
         }
 
         // Replacing null values by "Unknown"s
-        val unknown = df.na.fill(Map("city" -> "Unknown","impid" -> "Unknown","interests" -> "Unknown","network" -> "Unknown","type" -> "Unknown", "os" -> "Unknown"))
 
         // Removing all add with size null
-        val ds2 = unknown.filter(!col("size").getItem(0).isNull).filter(!col("size").getItem(1).isNull)
+        //val ds2 = unknown.filter(!col("size").getItem(0).isNull).filter(!col("size").getItem(1).isNull)
 
         // Gathering all windows phones together, set nulls & "Other" to "Unknown"
         val os = udf {
             (str: String) =>
-                val windows_pattern = new Regex("Windows(.*)")
+                val windows_pattern = new Regex("(.*)indows(.*)")
                 
                 str match {
                 case windows_pattern(x) => "Windows Phone"
                 case "ios" | "iOS" => "iOS"
-                case "Rim" | "Bada" | "WebOS"| "Symbian" | "BlackBerry" => "Unknown"
+                case "Rim" | "Bada" | "WebOS"| "Symbian" | "BlackBerry" | null => "Unknown"
                 case x => x.toLowerCase.capitalize
             }
         }
 
+        val df2 = df.select("appOrSite","interests","media","type","bidfloor","label","os","network","timestamp","size")
+
+        val df3 = 
+      {df2.withColumn("bidfloor", when(col("bidfloor").isNull, 3).otherwise(col("bidfloor")))
+      .withColumn("label", when(col("label") === true, 1).otherwise(0))
+      .withColumn("os", os(df2("os")))
+      .withColumn("network", network(df2("network")))
+      .withColumn("timestamp", epochToDate(df2("timestamp")))} 
 
         
 
-        ds2 // return clean dataframe
+        df3 // return clean dataframe
     }
 
     def epochToDate = udf((epochMillis: Long) => {
@@ -71,9 +77,10 @@ object clickinBad {
     val ds = spark.read.json("/home/cyp/IG/WI/data-students.json")
 
     val ds2 = preprocess(ds)
-    ds2.summary().show
+    //ds2.summary().show
+    ds2.groupBy("os").count().show()
 
-    epochToDate(ds2.col("timestamp").getItem(0))
+    print("ICIII \n" + epochToDate(ds2.col("timestamp").getItem(0)).toString() +"\n")
 
     spark.close()
   }
